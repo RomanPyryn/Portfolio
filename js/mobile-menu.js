832c9ab8773c282317a1fa0c1e2f70136039ab6b
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-11-03 21:40:13 +0200

    add menu corrections

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuItemRef = document.querySelectorAll("[data-item]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -10,5 +11,15 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+
+    menuItemRef.forEach((e) => {
+      e.addEventListener("click", () => {
+        if (mobileMenuRef.classList.value.includes("is-open")) {
+          menuBtnRef.classList.remove("is-open");
+          menuBtnRef.setAttribute("aria-expanded", !expanded);
+          mobileMenuRef.classList.remove("is-open");
+        }
+      });
+    });
   });
 })();
--- js/slider.js
+++ js/slider.js
@@ -2,16 +2,14 @@ var swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".slider-arrow-next",
     prevEl: ".slider-arrow-prev",
-	},
-	pagination: {
-    el: '.swiper-pagination',
-		type: 'bullets',
-		clickable: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
   },
   cssMode: true,
   mousewheel: true,
-	keyboard: true,
-	loop: true,
+  keyboard: true,
+  loop: true,
 });
-
-const next = document.querySelector(".swiper-button-next");
