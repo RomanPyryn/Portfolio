9b96757277f32bc72894c44f031398a2eafbcc82
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2023-08-14 20:15:56 +0300

    add aos, conecct it, additional hard skills, animate it, remove stars, SoftRyzen

---
+++ js/aos.js
@@ -0,0 +1,7 @@
+AOS.init({
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  duration: 600, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  anchorPlacement: 'top-center', // defines which position of the element regarding to window should trigger the animation
+});
